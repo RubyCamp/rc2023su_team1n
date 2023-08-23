d23aaca8fc5a1923855c976a967cfee1e6ae7642
LI WANTING <h229414@matsu.shimane-u.ac.jp>
2023-08-23 16:55:44 +0900

    dxrubyで画面にステージを表示させました

---
+++ README.md
@@ -0,0 +1,5 @@
+# rc2023su_team1
+RubyCamp2023 Summer Team1
+# 何でもいい
+なんでもいいそうです。
+# アイス大好き
--- player.rb
+++
@@ -1,5 +0,0 @@
-class Player < Character
-  def update(map)
-    update_new_position(map, @x + rand(3) - 1, @y + rand(3) - 1)
-  end
-end
